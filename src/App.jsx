95a1c500f206ce6d58243f7e28d0bd7cb8321afb
vaibhavi tyagi <vaibhavityagi5@gmail.com>
2023-09-26 23:04:25 +0530

    original commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { CssBaseline } from "@mui/material";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <ResponsiveAppBar />
+    </>
+  );
+}
+
+export default App;
